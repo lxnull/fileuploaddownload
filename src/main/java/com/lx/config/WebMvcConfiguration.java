d6bde7f386bae069af52dfa98dcca1d53e7bf8f4
lx <443199068@qq.com>
2022-03-25 11:16:37 +0800

    静态资源映射

--- src/main/java/com/lx/config/WebMvcConfiguration.java
+++ src/main/java/com/lx/config/WebMvcConfiguration.java
@@ -9,6 +9,8 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/file/**").addResourceLocations("file:G:\\JavaSpace\\Java进阶\\fileuploaddownload\\src\\main\\resources\\static\\file\\");
+        // 访问的路径：注意 ”/**“（相当于key或者映射）
+        // 上传资源的路径：真实路径最后 + ”/“（相当于val或者资源）
+        registry.addResourceHandler("/uploaded/**").addResourceLocations("file:G:\\JavaSpace\\Java进阶\\fileuploaddownload\\src\\main\\resources\\static\\file\\");
     }
 }
