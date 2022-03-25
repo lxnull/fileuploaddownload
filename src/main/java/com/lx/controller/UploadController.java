a040b34bb33580d77f06d2014d5efaf4f2d0669e
lx <443199068@qq.com>
2022-03-25 12:46:56 +0800

    静态资源映射-分布式资源存储（返回所有关于上传文件的信息map）

--- src/main/java/com/lx/controller/UploadController.java
+++ src/main/java/com/lx/controller/UploadController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class UploadController {
@@ -26,5 +28,17 @@ public class UploadController {
         String dir = request.getParameter("dir");
 
         return service.upload(multipartFile, dir);
+    }
+
+    @PostMapping("/uploadfilemap")
+        public Map<String,Object> uploadmap(@RequestParam("file") MultipartFile multipartFile, HttpServletRequest request) {
+        if (multipartFile.isEmpty()) {
+            return (Map<String, Object>) new HashMap<String,Object>().put("error","文件有误");
+        }
+
+        // 得到文件夹名，做隔离。
+        String dir = request.getParameter("dir");
+
+        return service.uploadMap(multipartFile, dir);
     }
 }
--- src/main/java/com/lx/service/UploadService.java
+++ src/main/java/com/lx/service/UploadService.java
@@ -8,6 +8,8 @@ import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 @Service
@@ -52,6 +54,47 @@ public class UploadService {
         } catch (IOException e) {
             e.printStackTrace();
             return "error";
+        }
+    }
+
+    // 返回完整资源
+    public Map<String,Object> uploadMap(MultipartFile multipartFile, String dir) {
+
+        HashMap<String, Object> map = new HashMap<>();
+
+        try {
+            // 1.获取文件真实名称
+            String originalFilename = multipartFile.getOriginalFilename();
+            // 2.切割文件名后缀
+            String suffix = originalFilename.substring(originalFilename.lastIndexOf("."));
+            // 3.生成唯一的文件名，防止同名文件导致覆盖
+            String newFilename = UUID.randomUUID().toString() + suffix;
+            // 4.跟据日期生成目录
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy/MM/dd");
+            String datePath = simpleDateFormat.format(new Date());
+            // 5.拼接完整的上传文件路径
+            File file = new File(uploadFolder + dir, datePath);
+            if (!file.exists()) {
+                file.mkdirs();
+            }
+            // 6.指定文件上传之后在服务器上的完整文件名
+            File targetFile = new File(file, newFilename);
+            // 7.开始上传
+            multipartFile.transferTo(targetFile);
+            // 8.返回完整的存储路径（存储到数据库）
+            String url =staticPath + staticPatternPath.substring(0,staticPatternPath.indexOf("*")) + dir + "/" + datePath + "/" + newFilename;
+
+            map.put("url",url);
+            map.put("size",multipartFile.getSize());
+            map.put("suffix",suffix);
+            map.put("filename",newFilename);
+            map.put("relativePath",dir + "/" + datePath + "/" +newFilename);
+            map.put("absolutePath",uploadFolder + dir + "/" + datePath + "/" +newFilename);
+            return map;
+        } catch (IOException e) {
+            e.printStackTrace();
+            map.put("error","error");
+            return map;
         }
     }
 }
